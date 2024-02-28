ec11c990a483e49243fe13d7964335314ffa94b8
Sarthak-Jog <161593741+Sarthak-Jog@users.noreply.github.com>
2024-02-28 17:10:54 +0530

    Add Addition.java and test.java file

---
+++ test.java
@@ -0,0 +1,33 @@
+class Test{
+       public static void main(String arg[]){
+
+            /*float number1 = 10.1f;  // variable
+            int number2 = 20;
+            float add;
+            add = number1 + number2;
+           System.out.println("Addition of Numbers");
+           System.out.println(add);
+           */
+
+           /*i
+            nt a = 10;
+           if(a>5 && a<10){
+           System.out.println("in If block");
+           }
+           else{
+           System.out.println("in else block");
+           }*/
+
+           int a = 10;
+           if(a>11){
+           System.out.println("in If block");
+           }
+           else if (a>5){
+           System.out.println("in elseif block");
+           }
+           else{
+           System.out.println("in else block");
+           }
+      }  // main end
+
+}  // class end
